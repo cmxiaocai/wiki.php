0e80f38e3928c3803377206bda0e3dee1505e6b3
cmxiaocai <cmxiaocai@163.com>
2016-07-14 18:34:28 +0800

    update

--- _includes/bootstrap.php
+++ _includes/bootstrap.php
@@ -4,5 +4,7 @@ include ROOTDIRECTORY_PATH.'_includes/Parsedown.php';
 include ROOTDIRECTORY_PATH.'_includes/simple_html_dom.php';
 include ROOTDIRECTORY_PATH.'_includes/match_title.class.php';
 include ROOTDIRECTORY_PATH.'_includes/parse_config.class.php';
+include ROOTDIRECTORY_PATH.'_includes/parse_filetype.class.php';
 include ROOTDIRECTORY_PATH.'_includes/make_lists.class.php';
 include ROOTDIRECTORY_PATH.'_includes/make_posts.class.php';
+
